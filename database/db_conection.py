21c8573df7a49f72db70534cf4dbe3332bbaa145
Tu Nombre <tu_email@ejemplo.com>
2026-09-21 23:18:22 -0500

    Agregar a la db_conection.py

--- database/db_conection.py
+++ database/db_conection.py
@@ -1,21 +1,37 @@
 import sqlite3
-from config import DB_PATH, SCHEMA_PATH
 import os
 
+from config import DB_PATH, SCHEMA_PATH
+
+
 def conectar_db():
-    # Crear la base de datos si no existe y tambien el directorio donde se encuentra
+
+    # Crear el directorio de la base de datos si no existe
     os.makedirs(os.path.dirname(DB_PATH), exist_ok=True)
+
+    # Crear e inicializar la base de datos si no existe
     if not os.path.exists(DB_PATH):
         inicializar_db()
-        
+
     return sqlite3.connect(DB_PATH, timeout=10)
 
+
 def inicializar_db():
-    with open(SCHEMA_PATH, 'r', encoding='utf-8') as f:
+
+    # Crear el directorio por seguridad
+    os.makedirs(os.path.dirname(DB_PATH), exist_ok=True)
+
+    # Leer el esquema SQL
+    with open(SCHEMA_PATH, "r", encoding="utf-8") as f:
         schema = f.read()
 
+    # Ejecutar el esquema
     with sqlite3.connect(DB_PATH, timeout=10) as conn:
+
         cursor = conn.cursor()
+
         cursor.executescript(schema)
+
         conn.commit()
-        print("DB inicializada correctamente.")
+
+    print("DB inicializada correctamente.")
